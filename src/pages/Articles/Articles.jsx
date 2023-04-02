e63ae37a11674fc49e800e94adfbb4d8bf98af27
Lukas Kliukas <lukaskliukas@gmail.com>
2023-04-02 20:03:56 +0300

    Home and Articles pages css

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Card from '../../components/Card/Card';
+import css from './Articles.module.css';
 
 const Articles = () => {
   return (
     <div>
       <Header />
+      <div className={css.container}>
+        <Card title='Mityba' content='Maistas yra skanu' />
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Card from './../../components/Card/Card';
+import css from './Home.module.css';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <h1>
-        Sport is a crucial aspect of human life that promotes physical fitness,
-        mental health, and social well-being. Engaging in physical activity can
-        have numerous benefits, such as improving cardiovascular health,
-        reducing the risk of chronic diseases, and enhancing cognitive function.
-        Furthermore, sports help individuals develop critical life skills such
-        as teamwork, communication, and leadership. Participating in sports also
-        provides a sense of community and belonging, bringing individuals
-        together and promoting social connections. Whether it's individual or
-        team-based sports, the benefits of participating in physical activity
-        are vast and undeniable. Therefore, it's essential to encourage and
-        promote participation in sports to improve overall health and
-        well-being.
-      </h1>
+      <div className={css.container}>
+        <Card
+          title='Apie sportą'
+          content='Sportas yra svarbus žmogaus gyvenimo aspektas, skatinantis fizinę formą, psichologinę sveikatą ir socialinį gerovę. Fizinis aktyvumas turi daugybę naudos, tokių kaip kardiovaskulinės sveikatos gerinimas, lėtinių ligų rizikos mažinimas ir kognityvinės funkcijos gerinimas. Be to, sportas padeda žmonėms įgyti kritinių gyvenimo įgūdžių, tokiais kaip komandos darbas, bendravimas ir lyderystė. Sporto dalyvavimas taip pat suteikia bendruomenės jausmą ir priklausomybę, sujungia žmones ir skatina socialinius ryšius. Nesvarbu, ar tai individualus, ar komandinis sportas, dalyvavimo fizinėje veikloje privalumai yra dideli ir nenuginčijami. Todėl svarbu skatinti ir skatinti dalyvavimą sporte, kad pagerintumėte bendrą sveikatą ir gerovę.'
+        />
+        <Card
+          title='Apie sportą 2'
+          content='Sportas yra svarbus žmogaus gyvenimo aspektas, skatinantis fizinę formą, psichologinę sveikatą ir socialinį gerovę. Fizinis aktyvumas turi daugybę naudos, tokių kaip kardiovaskulinės sveikatos gerinimas, lėtinių ligų rizikos mažinimas ir kognityvinės funkcijos gerinimas. Be to, sportas padeda žmonėms įgyti kritinių gyvenimo įgūdžių, tokiais kaip komandos darbas, bendravimas ir lyderystė. Sporto dalyvavimas taip pat suteikia bendruomenės jausmą ir priklausomybę, sujungia žmones ir skatina socialinius ryšius. Nesvarbu, ar tai individualus, ar komandinis sportas, dalyvavimo fizinėje veikloje privalumai yra dideli ir nenuginčijami. Todėl svarbu skatinti ir skatinti dalyvavimą sporte, kad pagerintumėte bendrą sveikatą ir gerovę.'
+        />
+        <Card
+          title='Apie sportą 3'
+          content='Sportas yra svarbus žmogaus gyvenimo aspektas, skatinantis fizinę formą, psichologinę sveikatą ir socialinį gerovę. Fizinis aktyvumas turi daugybę naudos, tokių kaip kardiovaskulinės sveikatos gerinimas, lėtinių ligų rizikos mažinimas ir kognityvinės funkcijos gerinimas. Be to, sportas padeda žmonėms įgyti kritinių gyvenimo įgūdžių, tokiais kaip komandos darbas, bendravimas ir lyderystė. Sporto dalyvavimas taip pat suteikia bendruomenės jausmą ir priklausomybę, sujungia žmones ir skatina socialinius ryšius. Nesvarbu, ar tai individualus, ar komandinis sportas, dalyvavimo fizinėje veikloje privalumai yra dideli ir nenuginčijami. Todėl svarbu skatinti ir skatinti dalyvavimą sporte, kad pagerintumėte bendrą sveikatą ir gerovę.'
+        />
+      </div>
     </div>
   );
 };
