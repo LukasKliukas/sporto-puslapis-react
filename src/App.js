ca6ecb2aac61791231262d114f6df2f117cff6c3
Lukas Kliukas <lukaskliukas@gmail.com>
2023-04-02 19:26:14 +0300

    React router dom created + normalize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Articles from './pages/Articles/Articles.jsx';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/articles' element={<Articles />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
