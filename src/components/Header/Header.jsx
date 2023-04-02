00fc6d3cc82e106083a670ad0d346d5b29fc0aad
Lukas Kliukas <lukaskliukas@gmail.com>
2023-04-02 20:19:13 +0300

    Header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,17 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className={css.header}>
-      Sport for everyone
-      <Link to='/'> Pagrindinis puslapis </Link>
-      <Link to='/articles'> Straipsniai </Link>
+      <div className={css.mainLogo}>
+        <p>SPORT FOR EVERYONE</p>
+      </div>
+      <div className={css.buttons}>
+        <Link className={css.link} to='/'>
+          Pagrindinis puslapis
+        </Link>
+        <Link className={css.link} to='/articles'>
+          Straipsniai
+        </Link>
+      </div>
     </div>
   );
 };
