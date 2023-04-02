b1f006df04727845630a42547f10fd800e567981
Lukas Kliukas <lukaskliukas@gmail.com>
2023-04-02 20:03:32 +0300

    Cards structure made

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import css from './Card.module.css';
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = (props) => {
+  return (
+    <div className={css.card}>
+      <img
+        src='https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=1380&t=st=1680454535~exp=1680455135~hmac=0b41e433a98b269d090db21fa78188471dfffc302006602096437fa6ea370a0a'
+        alt='logo'
+      />
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 };
 
 export default Card;
