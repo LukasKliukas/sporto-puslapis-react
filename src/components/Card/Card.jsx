8e9771d611f8988cb79ea0ba24c08fb5e7a35e66
Lukas Kliukas <lukaskliukas@gmail.com>
2023-04-06 12:56:43 +0300

    date added to a card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,6 +5,7 @@ const Card = (props) => {
   return (
     <div className={css.card}>
       <img src={props.src} alt='logo' />
+      <div className={css.date}>Data : {props.date}</div>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
     </div>
--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -9,6 +9,7 @@ const Articles = () => {
       <Header />
       <div className={css.container}>
         <Card
+          date='2023-04-06'
           title='Mityba'
           content='Maistas yra skanu'
           src='https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=1380&t=st=1680454535~exp=1680455135~hmac=0b41e433a98b269d090db21fa78188471dfffc302006602096437fa6ea370a0a'
