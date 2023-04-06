f982bb0870a47779cc35a9c5e4d6b329abe6f5b9
Lukas Kliukas <lukaskliukas@gmail.com>
2023-04-06 12:41:38 +0300

    Src prop added to card

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -8,7 +8,11 @@ const Articles = () => {
     <div>
       <Header />
       <div className={css.container}>
-        <Card title='Mityba' content='Maistas yra skanu' />
+        <Card
+          title='Mityba'
+          content='Maistas yra skanu'
+          src='https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=1380&t=st=1680454535~exp=1680455135~hmac=0b41e433a98b269d090db21fa78188471dfffc302006602096437fa6ea370a0a'
+        />
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,14 +9,17 @@ const Home = () => {
       <Header />
       <div className={css.container}>
         <Card
+          src='https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=1380&t=st=1680454535~exp=1680455135~hmac=0b41e433a98b269d090db21fa78188471dfffc302006602096437fa6ea370a0a'
           title='Apie sportą'
           content='Sportas yra svarbus žmogaus gyvenimo aspektas, skatinantis fizinę formą, psichologinę sveikatą ir socialinį gerovę. Fizinis aktyvumas turi daugybę naudos, tokių kaip kardiovaskulinės sveikatos gerinimas, lėtinių ligų rizikos mažinimas ir kognityvinės funkcijos gerinimas. Be to, sportas padeda žmonėms įgyti kritinių gyvenimo įgūdžių, tokiais kaip komandos darbas, bendravimas ir lyderystė. Sporto dalyvavimas taip pat suteikia bendruomenės jausmą ir priklausomybę, sujungia žmones ir skatina socialinius ryšius. Nesvarbu, ar tai individualus, ar komandinis sportas, dalyvavimo fizinėje veikloje privalumai yra dideli ir nenuginčijami. Todėl svarbu skatinti ir skatinti dalyvavimą sporte, kad pagerintumėte bendrą sveikatą ir gerovę.'
         />
         <Card
+          src='https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=1380&t=st=1680454535~exp=1680455135~hmac=0b41e433a98b269d090db21fa78188471dfffc302006602096437fa6ea370a0a'
           title='Apie sportą 2'
           content='Sportas yra svarbus žmogaus gyvenimo aspektas, skatinantis fizinę formą, psichologinę sveikatą ir socialinį gerovę. Fizinis aktyvumas turi daugybę naudos, tokių kaip kardiovaskulinės sveikatos gerinimas, lėtinių ligų rizikos mažinimas ir kognityvinės funkcijos gerinimas. Be to, sportas padeda žmonėms įgyti kritinių gyvenimo įgūdžių, tokiais kaip komandos darbas, bendravimas ir lyderystė. Sporto dalyvavimas taip pat suteikia bendruomenės jausmą ir priklausomybę, sujungia žmones ir skatina socialinius ryšius. Nesvarbu, ar tai individualus, ar komandinis sportas, dalyvavimo fizinėje veikloje privalumai yra dideli ir nenuginčijami. Todėl svarbu skatinti ir skatinti dalyvavimą sporte, kad pagerintumėte bendrą sveikatą ir gerovę.'
         />
         <Card
+          src='https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=1380&t=st=1680454535~exp=1680455135~hmac=0b41e433a98b269d090db21fa78188471dfffc302006602096437fa6ea370a0a'
           title='Apie sportą 3'
           content='Sportas yra svarbus žmogaus gyvenimo aspektas, skatinantis fizinę formą, psichologinę sveikatą ir socialinį gerovę. Fizinis aktyvumas turi daugybę naudos, tokių kaip kardiovaskulinės sveikatos gerinimas, lėtinių ligų rizikos mažinimas ir kognityvinės funkcijos gerinimas. Be to, sportas padeda žmonėms įgyti kritinių gyvenimo įgūdžių, tokiais kaip komandos darbas, bendravimas ir lyderystė. Sporto dalyvavimas taip pat suteikia bendruomenės jausmą ir priklausomybę, sujungia žmones ir skatina socialinius ryšius. Nesvarbu, ar tai individualus, ar komandinis sportas, dalyvavimo fizinėje veikloje privalumai yra dideli ir nenuginčijami. Todėl svarbu skatinti ir skatinti dalyvavimą sporte, kad pagerintumėte bendrą sveikatą ir gerovę.'
         />
